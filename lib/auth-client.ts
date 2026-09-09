9e425f756ab72aed0bec6908b920258d6531c4a2
azzaahlem <bsazzaahlem@gmail.com>
2026-09-09 10:46:40 +0200

    fix better auth client url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,13 +1,13 @@
-import { createAuthClient } from 'better-auth/react';
-import { adminClient } from 'better-auth/client/plugins';
-import { inferAdditionalFields } from 'better-auth/client/plugins';
-// ⚠️ CORRECTION : Ajouter 'type' ici pour éviter de charger le serveur côté client
-import type { auth } from '@/lib/auth';
+import { createAuthClient } from "better-auth/react";
+import { adminClient } from "better-auth/client/plugins";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "@/lib/auth";
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
-
-  plugins: [inferAdditionalFields<typeof auth>(), adminClient()],
+  plugins: [
+    inferAdditionalFields<typeof auth>(),
+    adminClient(),
+  ],
 });
 
 export const { useSession } = authClient;
