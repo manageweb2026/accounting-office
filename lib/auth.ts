3195b75b98596edaf6929417a24dbcaeefb0eb59
azzaahlem <bsazzaahlem@gmail.com>
2026-09-10 08:20:18 +0200

    fix better auth origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,7 +13,14 @@ export const auth = betterAuth({
 
   secret: process.env.BETTER_AUTH_SECRET!,
 
-  baseURL: process.env.BETTER_AUTH_URL!,
+    baseURL: {
+    allowedHosts: [
+      "*.vercel.app",
+      "localhost:*",
+    ],
+    protocol: "auto",
+    fallback: process.env.BETTER_AUTH_URL!,
+      },
 
   
   emailAndPassword: {
